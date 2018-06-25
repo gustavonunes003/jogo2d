1a028a7139bce00198fdaafec7ca9ec9d2ae0bb0
Gustavo Nunes <gustnunes.mp4@gmail.com>
2018-06-25 11:18:17 -0300

    Tarefa 2 finalizada

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -10,11 +10,15 @@ public float jumpSpeed = 3000f;
 
 Rigidbody2D rb;
 SpriteRenderer sr;
+Animator anim;
+
+bool isJumping = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
 		
 	}
 	
@@ -37,7 +41,7 @@ if (Input.GetButtonDown("Jump"))
 {
 Jump();
 }
-		
+	ShowFalling();	
 }
 
 
@@ -51,15 +55,33 @@ if (speed < 0f){
 else if (speed > 0f){
 	sr.flipX = false;
 }
+if(!isJumping){
+anim.SetInteger("State", 2);
+}
 }
 
 void StopMovingHorizontal() {
 rb.velocity = new Vector2(0f, rb.velocity.y);
+if(!isJumping){
+anim.SetInteger("State", 0);
+}
+}
+
+void ShowFalling(){
+	if (rb.velocity.y < 0f) {
+		anim.SetInteger("State", 3);
+	}
 }
 
 void Jump() 
 {
+isJumping = true;	
 rb.AddForce(new Vector2(0f, jumpSpeed));
+anim.SetInteger("State", 1);
 }
-
+void OnCollisionEnter2D(Collision2D other){
+	if(other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+		isJumping = false;
+	}
+}
 }
